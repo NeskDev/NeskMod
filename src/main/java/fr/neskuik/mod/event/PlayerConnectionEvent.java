96dff631320d7607125c22f45d9f2e66eb29d61b
neskuik_k <giraudeau.malone@icloud.com>
2025-05-24 14:22:01 +0200

    feat: add player quit handling to disable moderation mode in PlayerConnectionEvent

--- src/main/java/fr/neskuik/mod/event/PlayerConnectionEvent.java
+++ src/main/java/fr/neskuik/mod/event/PlayerConnectionEvent.java
@@ -1,15 +1,27 @@
 package fr.neskuik.mod.event;
 
 import fr.neskuik.mod.api.CheckUpdates;
+import fr.neskuik.mod.commands.ModCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerConnectionEvent implements Listener {
+
+    private ModCommand mod;
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         CheckUpdates.checkForUpdate();
+    }
 
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        Player player = e.getPlayer();
+        if (mod.isInModMode(player)) {
+            mod.disableMod(player);
+        }
     }
 }
